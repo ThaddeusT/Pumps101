4823418fb6ee36ad4260fec6d22b6faa0c66651a
mwhite14 <mwhite14@ksu.edu>
2014-10-27 22:02:10 -0500

    All calculations complete for levels we will be doing

--- Pumps101/Pumps101/Repositories/Calculations.cs
+++ Pumps101/Pumps101/Repositories/Calculations.cs
@@ -5,7 +5,6 @@ using System.Web;
 
 namespace Pumps101.Repositories
 {
-
     // lvl 1-6 is getting hp
     // level 7 is getting hp and Net Positive Suction
     public class Calculations
@@ -17,48 +16,71 @@ namespace Pumps101.Repositories
         private double _B;
         private double _C;
         private double _volumetricFlowRate = 0;
-        // model
-        private double _diam;		// in inches
-        private double _density;	// in (lbm/ft^3)
-        private double _time; 	// in hours
-        private double _volume;  	// in cubic feet
-        private int[] _tankElevation; 	// in feet ( everything is 200 - 800ft higher than 1st tank)
-        private int[] _vertLength;      // in feet ( ranging 500ft - 1500ft, this is the horizontal pipe segments)        
-        private double[] _tankPressure;		// psig
-        private double _viscosity;      //  lbm/feet second
-        private double _vaporPressure;   // psi
+
+        // 
+        // MODEL
+        //
+
         private int _maxNumberOfChances;
 
-        // answers
+        //lvl 1 - 10
+        private double _diam;		// in inches
+        private double _density;	// in (lbm/ft^3)
+        private double _time; 	    // in hours
+        private double _volume;  	// in cubic feet
+
+        //lvl 2 - 10
+        private int[] _tankElevation; 	    // in feet ( everything is 200 - 800ft higher than 1st tank)
+
+        // lvl 3 - 10
+        private double[] _tankPressure;		// psig
+
+        //lvl 4 - 10
+        private double _viscosity;          //  lbm/feet second
+        private int[] _vertLength;          // in feet ( ranging 500ft - 1500ft, this is the horizontal pipe segments) 
+        
+        //lvl 5
+        //give them table to calc ev
+
+        //lvl 6 - 10
+        private double _efficencyFactor;    // %
+
+        //lvl 7 - 10
+        private double _vaporPressure;      // psi
+
+        // lvl 8 - 10
+        // give them choice in pump to use
+        // "Diaphragm" "Rotary" "Centrifugal"  ... it will never be Diaphram from our setup
+
+        // lvl 9 
+        // materials, skip for now
+
+        // lvl 10
+        // material can be "Cast Iron" "Cast Steel" "Stainless Steel" "Nickel Alloy"
+        private string _material;
+
+        
+        //
+        // ANSWERS
+        //
         // level 1-6 
         private double _hp_correct;  // once they check the value once it will be set so they don't have to recalculate
         // level 7 - 10
         private double _NPSH_correct;
         // level 8 - 10
-        private string _pumpType;
-
-        public double getPipeDiam() { return _diam; }
-        public double getLiquidDensity() { return _density; }
-        public double getTime() { return _time; }
-        public double getVolume() { return _volume; }
-        public int[] getTankElevations() { return _tankElevation; }
-        public double getCorrectHP() { return _hpCorrect; }
-        public double[] getTankPressure() { return _tankPressure; }
-        public bool isLvlSet() { return _lvlIsSet; }
+        private string _pumpType_correct;
+        // level 10
+        private double _cost_correct;
 
 
         Calculations(int level, int chances)
 	    {
             _level = level;
-		    if(level > 0 && level < 4)
-		    {
-			    setLevel(chances);
-		    }
+		    setLevel(chances);
 	    }
 
 
         // The Calculations // 
-
 
         /// <summary>
         ///  Gets correct horse power based of the values dynamically set for the level
@@ -105,7 +127,14 @@ namespace Pumps101.Repositories
             }
 
             // returns correct hp
-            return convertWorkToHorsePower(workPerMass, _volumetricFlowRate, _density);
+            if(_level > 5)
+            {
+                return convertWorkToHorsePower(workPerMass, _volumetricFlowRate, _density) / _efficencyFactor;
+            }
+            else 
+            { 
+                return convertWorkToHorsePower(workPerMass, _volumetricFlowRate, _density);
+            }
         }
 
         // used by level 2 - 10
@@ -118,26 +147,21 @@ namespace Pumps101.Repositories
         private double getPressureFromWork()
         {
             double deltaP = _tankPressure[1] - _tankPressure[0];
-
             return (deltaP / _density) * 144 * 32.174;
-
         }
 
         // This will always work for getting power!! 
         // used by level 1-10
         private double convertWorkToHorsePower(double workPerMass, double volumetricFlowRate, double density)
         {
-            double hp = (((workPerMass / 32.174) * volumetricFlowRate) * density) / 550;
-            return hp;
+            return (((workPerMass / 32.174) * volumetricFlowRate) * density) / 550;
         }
 
         // used by level 1-10
         private double getWorkFromVelocities(double v1, double v2)
         {
-            double work = .5 * (Math.Pow(v2, 2) - Math.Pow(v1, 2));
-            return work;
+            return .5 * (Math.Pow(v2, 2) - Math.Pow(v1, 2));
         }
-
 
         // Net Positive Suction Head
         // Level 7 - 10
@@ -164,16 +188,108 @@ namespace Pumps101.Repositories
             }
         }
 
+        // level 10
+        private double getCost()
+        {
+            double baseCost = 0;
+            double pressure = (_hp_correct * 1641.61) / (_volumetricFlowRate * 32.174);
+
+            if(_pumpType_correct.CompareTo("Rotary") == 0)
+            {
+                baseCost = 34933 * Math.Pow((_volumetricFlowRate * 0.02832), 0.4041);
+                if (_material.CompareTo("Cast Iron") == 0)
+                {
+                    // do nothing
+                }
+                else if (_material.CompareTo("Cast Steel") == 0)
+                {
+                    baseCost = baseCost * 1.4;
+                }
+                else if (_material.CompareTo("Stainless Steel") == 0)
+                {
+                    baseCost = baseCost * 2;
+                }
+                else
+                {
+                    baseCost = baseCost * 4;
+                }
+
+                if (pressure <= 1035)
+                {
+                    return baseCost;
+                }
+                else if (pressure <= 5000)
+                {
+                    return baseCost * 2;
+                }
+                else if (pressure <= 10000)
+                {
+                    return baseCost * 2.5;
+                }
+                else if (pressure <= 20000)
+                {
+                    return baseCost * 3.1;
+                }
+                else
+                {
+                    return baseCost * 3.4;
+                }
+            }
+            else if(_pumpType_correct.CompareTo("Centrifugal") == 0)
+            {
+               baseCost = 18116 * Math.Pow((_volumetricFlowRate * 0.02832), 0.3638);
+
+               if (_material.CompareTo("Cast Iron") == 0)
+               {
+                   // do nothing
+               }
+               else if (_material.CompareTo("Cast Steel") == 0)
+               {
+                   baseCost = baseCost * 1.8;
+               }
+               else if (_material.CompareTo("Stainless Steel") == 0)
+               {
+                   baseCost = baseCost * 2.4;
+               }
+               else
+               {
+                   baseCost = baseCost * 5;
+               }
+
+               if (pressure <= 1035)
+               {
+                   return baseCost;
+               }
+               else if (pressure <= 5000)
+               {
+                   return baseCost * 2.1;
+               }
+               else if (pressure <= 10000)
+               {
+                   return baseCost * 2.8;
+               }
+               else if (pressure <= 20000)
+               {
+                   return baseCost * 3.5;
+               }
+               else
+               {
+                   return baseCost * 4;
+               }
+            }
+            return baseCost;
+
+        }
+
         // Setting Level //
 
-         /// <summary>
-         /// Currently, dynamically sets values for levels 1-3 and gets correct HP
-         /// </summary>
-         /// <param name="chances">the number of chance they have to get it correct</param>
+        /// <summary>
+        /// Currently, dynamically sets values for levels 1-10 and gets correct HP and all other correct values
+        /// </summary>
+        /// <param name="chances">the number of chance they have to get it correct</param>
         private void setLevel(int chances)
         {
             Random rn = new Random();
-
             if (_level < 4) { 
                 _diams = new double[] { 0.75, 1, 1.25, 1.5 };
                 _diam = _diams[rn.Next(4)];
@@ -182,7 +298,7 @@ namespace Pumps101.Repositories
             else
             {
                 _diams = new double[] { 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5};
-                _diam = _diams[rn.Next(7)];
+                _diam = _diams[rn.Next(8)];
                 switch ((int)(_diam*10))
                 {
                     case 15:
@@ -229,50 +345,44 @@ namespace Pumps101.Repositories
 
             }
             _density = 62.4;
-
+            _efficencyFactor = rn.Next(65, 86) * 0.001;
             _tankElevation = new int[2];
             _tankPressure = new double[2];
             for (int i = 0; i < 2; i++)
             {
                 if (i != 0)
                 {
-                    _tankElevation[i] = _tankElevation[0] + (200 + rn.Next(800));
-                    _tankPressure[i] = (double)Math.Round((_tankPressure[0] + (double)rn.Next(5,24) + rn.NextDouble()) * 10) / 10;
+                    _tankElevation[i] = _tankElevation[0] + (200 + rn.Next(801));
+                    _tankPressure[i] = (double)Math.Round((_tankPressure[0] + (double)rn.Next(5,25) + rn.NextDouble()) * 10) / 10;
                 }
                 else
                 {
-                    _tankElevation[i] = rn.Next(350) + 150;
-                    _tankPressure[i] = (double)Math.Round((double)(rn.Next(5,49) + rn.NextDouble()) * 10) / 10;
+                    _tankElevation[i] = rn.Next(351) + 150;
+                    _tankPressure[i] = (double)Math.Round((double)(rn.Next(5,50) + rn.NextDouble()) * 10) / 10;
                 }
 
             }
-            _vaporPressure = _tankPressure[0] - rn.Next(8, 15);
+            _vaporPressure = _tankPressure[0] - rn.Next(8, 16);
             if (_vaporPressure < 3) { _vaporPressure = 3; }
-
-            _vertLength[0] = rn.Next(500, 1500);
-            _vertLength[1] = rn.Next(500, 1500);
-            _viscosity = rn.Next(526, 1201) * 0.0001;
-
+            _vertLength[0] = rn.Next(500, 1501);
+            _vertLength[1] = rn.Next(500, 1501);
+            _viscosity = rn.Next(526, 1202) * 0.0001;
             double timeUnrounded = 0;
 
             // use higher volume and less time for later levels
             if (_level > 7)
             {
                 timeUnrounded = (rn.NextDouble() * (1.25) + 0.25);
-                _volume = rn.Next(5000, 1000);
+                _volume = rn.Next(5000, 10001);
             }
             else
             {
                 timeUnrounded = (rn.NextDouble() * (2 - 0.75) + 0.75);
-                _volume = rn.Next(2000, 5000);
+                _volume = rn.Next(2000, 5001);
             }
             _time = ((int)Math.Round(timeUnrounded * 100)) / 100.0;
-            
-
-
             _lvlIsSet = true;
             _maxNumberOfChances = 3;
-
             _hp_correct = getHorsePower((_level > 1), (_level > 2), (_level > 3 && _level < 6));
             
             if(_level > 6){
@@ -280,9 +390,15 @@ namespace Pumps101.Repositories
             }
             if(_level > 7)
             {
-                _pumpType = getPumpToUse();
+                _pumpType_correct = getPumpToUse();
             }
-            
+            // set material for level 10 since there is no level 9 where they select it themself
+            if(_level > 9)
+            {
+                string[] materialArr = new string[] { "Cast Iron", "Cast Steel", "Stainless Steel", "Nickel Alloy" };
+                _material = materialArr[rn.Next(4)];
+                _cost_correct = getCost();
+            }
         }
     }
 }
