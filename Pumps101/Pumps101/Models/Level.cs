fdae2de83d907a60e79da2ad8bbe9af9bc5f26cb
mwhite14 <mwhite14@ksu.edu>
2014-10-31 11:21:21 -0500

    Outline for LevelsCOmplete Model

---
+++ Pumps101/Pumps101/Models/Level.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pumps101.Models
+{
+    public class Level
+    {
+        #region Fields
+        private int _level;
+        private bool _isLocked;
+        private int _stars;
+        #endregion
+
+        #region Constructors
+        Level(int lv, int stars, bool isLocked)
+        {
+            _level = lv;
+            _stars = stars;
+            _isLocked = isLocked;
+        }
+        #endregion
+
+        #region Properties
+        public int Level
+        {
+            get { return _level; }
+        }
+        public bool Locked
+        {
+            get { return _isLocked; }
+        }
+        public int Stars
+        {
+            get { return _stars; }
+        }
+        #endregion
+    }
+}
---
+++ Pumps101/Pumps101/Models/LevelsComplete.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Pumps101.Models
+{
+    public class LevelsComplete
+    {
+        #region Fields 
+
+        List<Level> _levels = new List<Level>();
+
+        #endregion
+
+
+        #region Constructors
+
+        public LevelsComplete(Guid user)
+        {
+            // TODO: get all levels for the user
+            // if level hasnt been created in database, add it
+            // set levels
+        }
+
+        #endregion
+
+        #region Properties
+
+        public List<Level> Levels
+        {
+            get { return _levels; }
+        }
+
+        #endregion
+
+    }
+}
