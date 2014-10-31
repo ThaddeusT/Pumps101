25eb91f926416f95c5fea219b3c3d5de91e35499
mwhite14 <mwhite14@ksu.edu>
2014-10-31 12:16:33 -0500

    Update Application Start to Use correct database for login.

--- Pumps101/Pumps101/Models/IdentityModels.cs
+++ Pumps101/Pumps101/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace Pumps101.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("DefaultConnection")
         {
         }
 
